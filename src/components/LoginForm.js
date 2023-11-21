c813b807d491f3dc5d7d4f9ef78064fc18985c6a
Birçe Yakut <134504827+yakutbirce@users.noreply.github.com>
2023-11-21 23:56:06 +0300

    Update LoginForm.js

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -14,7 +14,6 @@ const LoginForm = () => {
             password: Yup.string().required('Şifre zorunludur'),
         }),
         onSubmit: (values) => {
-            // Giriş işlemini burada gerçekleştirebilirsiniz.
             console.log('Giriş Başarılı!', values);
         },
     });
